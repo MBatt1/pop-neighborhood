320083c47c3cc0313f7ca87732a4c4aea54f53ed
MBatt1 <53229958+MBatt1@users.noreply.github.com>
2022-04-21 14:50:00 -0500

    fill over time

--- scripts/main.js
+++ scripts/main.js
@@ -2,56 +2,190 @@
 var canvas = document.getElementById("mainCanvas");
 var ctx = canvas.getContext("2d");
 
-var tiles = []
+
+var tiles = [];
+
+var grid = [];
+var frontier = [];
+
 
 // Fill the grid with tiles
 function fillGrid() {
-    var screenTileWidth = canvas.width/100+1;
-    var screenTileHeight = canvas.height/25+1;
+	
+	var screenTileWidth = canvas.width/100+1;
+	var screenTileHeight = canvas.height/25+1;
 
-    var grid = [];
+    grid = [];
     // fill grid with emptys
     for (var yPlace = 0; yPlace < screenTileHeight; yPlace++) {
         var temp = [];
         for (var xPlace = 0; xPlace < screenTileWidth; xPlace++) {
-            temp[xPlace] = 0;
+            temp[xPlace] = -1;
         }
         grid[yPlace] = temp;
     }
     
-    var startX = Math.floor(Math.random() * screenTileWidth);
-    var startY = Math.floor(Math.random() * screenTileHeight);
+    var startX = 4; //Math.floor(Math.random() * screenTileWidth);
+    var startY = 12; //Math.floor(Math.random() * screenTileHeight);
 
-    var frontier = [{x:startX, y:startY}]
+    frontier = [{x:startX, y:startY}]
+	grid[startY][startX] = 0;
     
-    while (frontier.length > 0) {
+   
+    var ticker = setInterval(advanceDraw, 200);
+    
+};
+
+function advanceDraw() {
+	if (frontier.length > 0) {
         var front = frontier.pop();
         var frontTile = tiles[grid[front.y][front.x]];
         
-        if (front.y % 2 == 0) {
-            if (front.y > 0) {
-                frontier.push({x:front.x-1, y:front.y-1});
-                grid[front.y-1][front.x-1] = randomArrayChoice(frontTile.validConnections.u1);
-            }
-        } else {
-            if (front.y > 0) {
-                frontier.push({x:front.x, y:front.y-1});
-                grid[front.y-1][front.x] = randomArrayChoice(frontTile.validConnections.u1);
-            }
-        }
+		var upLeft = getUpLeft(front);
+		var upRight = getUpRight(front);
+		var downLeft = getDownLeft(front);
+		var downRight = getDownRight(front);
+		
+		var newTiles = [];
+		
+		if (grid[upLeft.y] && grid[upLeft.y][upLeft.x] && getTile(grid, upLeft) == -1) {
+			frontier.push({x:upLeft.x, y:upLeft.y});
+			var newTiles = frontTile.validConnections.u1;
+			newTiles.sort(() => Math.random() - 0.5);
+			var foundTile = null;
+			while (!foundTile) {
+				var temp = newTiles.pop();
+				if (tileFits(upLeft, temp)) {
+					grid[upLeft.y][upLeft.x] = temp;
+					//alert("placing " + temp)
+					foundTile = true;
+				}
+			}
+		}
+		if (grid[upRight.y] && grid[upRight.y][upRight.x] && getTile(grid, upRight) == -1) {
+			frontier.push({x:upRight.x, y:upRight.y});
+			var newTiles = frontTile.validConnections.u2;
+			newTiles.sort(() => Math.random() - 0.5);
+			var foundTile = null;
+			while (!foundTile) {
+				var temp = newTiles.pop();
+				if (tileFits(upRight, temp)) {
+					grid[upRight.y][upRight.x] = temp;
+					//alert("placing " + temp)
+					foundTile = true;
+				}
+			}
+		}
+		if (grid[downLeft.y] && grid[downLeft.y][downLeft.x] && getTile(grid, downLeft) == -1) {
+			frontier.push({x:downLeft.x, y:downLeft.y});
+			var newTiles = frontTile.validConnections.d1;
+			newTiles.sort(() => Math.random() - 0.5);
+			var foundTile = null;
+			while (!foundTile) {
+				var temp = newTiles.pop();
+				if (tileFits(downLeft, temp)) {
+					grid[downLeft.y][downLeft.x] = temp;
+					//alert("placing " + temp)
+					foundTile = true;
+				}
+			}
+		}
+		if (grid[downRight.y] && grid[downRight.y][downRight.x] && getTile(grid, downRight) == -1) {
+			frontier.push({x:downRight.x, y:downRight.y});
+			var newTiles = frontTile.validConnections.d2;
+			newTiles.sort(() => Math.random() - 0.5);
+			var foundTile = null;
+			while (!foundTile) {
+				var temp = newTiles.pop();
+				if (tileFits(downRight, temp)) {
+					grid[downRight.y][downRight.x] = temp;
+					//alert("placing " + temp)
+					foundTile = true;
+				}
+			}
+		}
+		frontier.sort(() => Math.random() - 0.5);
+		
+		drawGrid();
     }
-    
-    for (var yPlace = 0; yPlace < screenTileHeight; yPlace++) {
-        for (var xPlace = 0; xPlace < screenTileWidth; xPlace++) {
-            //if (grid[yPlace][xPlace] == 0) continue;
+}
+
+function tileFits(coords, tileIndex) {
+	var upLeft = getUpLeft(coords);
+	var upRight = getUpRight(coords);
+	var downLeft = getDownLeft(coords);
+	var downRight = getDownRight(coords);
+	
+	if (getTile(grid, upLeft) != -1 && !tiles[tileIndex].validConnections.u1.includes(getTile(grid, upLeft))) {
+		return false;
+	}
+	if (getTile(grid, upRight) != -1 && !tiles[tileIndex].validConnections.u2.includes(getTile(grid, upRight))) {
+		return false;
+	}
+	if (getTile(grid, downLeft) != -1 && !tiles[tileIndex].validConnections.d1.includes(getTile(grid, downLeft))) {
+		return false;
+	}
+	if (getTile(grid, downRight) != -1 && !tiles[tileIndex].validConnections.d2.includes(getTile(grid, downRight))) {
+		return false;
+	}
+	return true;
+}
+
+function drawGrid() {
+	for (var yPlace = 0; yPlace < grid.length; yPlace++) {
+        for (var xPlace = 0; xPlace < grid[0].length; xPlace++) {
+            if (grid[yPlace][xPlace] == -1) continue;
             var xOffset = xPlace*200;
             if (yPlace % 2 == 0) {
                 xOffset -= 100;
             }
-            ctx.drawImage(randomArrayChoice(tiles).image, xOffset, yPlace*50-250);
-            //ctx.drawImage(tiles[grid[yPlace][xPlace]].image, xOffset, yPlace*50-250);
+            ctx.drawImage(tiles[grid[yPlace][xPlace]].image, xOffset, yPlace*50-250);
         }
     }
+}
+
+function getUpLeft(coords) {
+	var sX = coords.x;
+	var sY = coords.y;
+	if (sY % 2 == 0) {
+		return {x:sX-1, y:sY-1};
+	} else {
+		return {x:sX, y:sY-1};
+	}
+};
+
+function getUpRight(coords) {
+	var sX = coords.x;
+	var sY = coords.y;
+	if (sY % 2 == 0) {
+		return {x:sX, y:sY-1};
+	} else {
+		return {x:sX+1, y:sY-1};
+	}
+};
+
+function getDownLeft(coords) {
+	var sX = coords.x;
+	var sY = coords.y;
+	if (sY % 2 == 0) {
+		return {x:sX-1, y:sY+1};
+	} else {
+		return {x:sX, y:sY+1};
+	}
+};
+
+function getDownRight(coords) {
+	var sX = coords.x;
+	var sY = coords.y;
+	if (sY % 2 == 0) {
+		return {x:sX, y:sY+1};
+	} else {
+		return {x:sX+1, y:sY+1};
+	}
+};
+
+function getTile(fromGrid, coords) {
+	return fromGrid[coords.y][coords.x];
 };
 
 // {u1:[], u2:[], d1:[], d2:[]}
@@ -88,6 +222,14 @@ function windowResize() {
 function randomArrayChoice(arr) {
     return arr[Math.floor(Math.random() * arr.length)];
 };
+
+function timedFill() {
+	return new Promise(resolve => {
+    setTimeout(() => {
+      resolve('resolved');
+    }, 20000);
+  });
+}
 
 // Main function
 function main() {
