0971ea419d3676c07e9aded640a117b34f9e0999
24Chrome <m.batt1@outlook.com>
2022-04-21 12:49:48 -0500

    further work :(

--- scripts/main.js
+++ scripts/main.js
@@ -14,25 +14,42 @@ function fillGrid() {
     for (var yPlace = 0; yPlace < screenTileHeight; yPlace++) {
         var temp = [];
         for (var xPlace = 0; xPlace < screenTileWidth; xPlace++) {
-            temp[xPlace] = null;
+            temp[xPlace] = 0;
         }
         grid[yPlace] = temp;
     }
+    
+    var startX = Math.floor(Math.random() * screenTileWidth);
+    var startY = Math.floor(Math.random() * screenTileHeight);
 
+    var frontier = [{x:startX, y:startY}]
+    
+    while (frontier.length > 0) {
+        var front = frontier.pop();
+        var frontTile = tiles[grid[front.y][front.x]];
+        
+        if (front.y % 2 == 0) {
+            if (front.y > 0) {
+                frontier.push({x:front.x-1, y:front.y-1});
+                grid[front.y-1][front.x-1] = randomArrayChoice(frontTile.validConnections.u1);
+            }
+        } else {
+            if (front.y > 0) {
+                frontier.push({x:front.x, y:front.y-1});
+                grid[front.y-1][front.x] = randomArrayChoice(frontTile.validConnections.u1);
+            }
+        }
+    }
+    
     for (var yPlace = 0; yPlace < screenTileHeight; yPlace++) {
         for (var xPlace = 0; xPlace < screenTileWidth; xPlace++) {
+            //if (grid[yPlace][xPlace] == 0) continue;
             var xOffset = xPlace*200;
             if (yPlace % 2 == 0) {
                 xOffset -= 100;
             }
-            var rand = Math.random();
-            if (rand < 0.6) {
-                ctx.drawImage(tiles[0].image, xOffset, yPlace*50-250);
-            } else if (rand < 0.8){
-                ctx.drawImage(tiles[1].image, xOffset, yPlace*50-250);
-            } else {
-                ctx.drawImage(tiles[2].image, xOffset, yPlace*50-250);
-            }
+            ctx.drawImage(randomArrayChoice(tiles).image, xOffset, yPlace*50-250);
+            //ctx.drawImage(tiles[grid[yPlace][xPlace]].image, xOffset, yPlace*50-250);
         }
     }
 };
@@ -44,12 +61,12 @@ function populateTiles() {
     tiles[2] = createImage("2_house2_tile", {u1:[0, 1, 2, 4, 5, 7], u2:[0, 1, 2, 3, 6, 7], d1:[0, 1, 2, 3, 5, 8], d2:[0, 1, 2, 4, 6, 8]});
     
     tiles[3] = createImage("3_straight_road_1_tile", {u1: [3, 6, 8], u2:[0, 1, 2, 3, 6, 7], d1:[0, 1, 2, 3, 5, 8], d2:[3, 5, 7]});  // 
-    tiles[4] = createImage("4_straight_road_2_tile", {u1: [0, 1, 2, 4, 6, 8], u2:[4, 5, 8], d1:[4, 6, 7], d2:[0, 1, 2, 4, 6, 8]});  // /
+    tiles[4] = createImage("4_straight_road_2_tile", {u1: [0, 1, 2, 4, 7], u2:[4, 5, 8], d1:[4, 6, 7], d2:[0, 1, 2, 4, 6, 8]});  // /
     
     tiles[5] = createImage("5_curved_road_1_tile", {u1: [3, 6, 8], u2:[0, 1, 2, 3, 6, 7], d1:[4, 6, 7], d2:[0, 1, 2, 4, 6, 8]});    // >
-    tiles[6] = createImage("6_curved_road_2_tile", {u1: [0, 1, 2, 4, 6, 8], u2:[4, 5, 8], d1:[0, 1, 2, 3, 5, 8], d2:[3, 5, 7]});    // <
+    tiles[6] = createImage("6_curved_road_2_tile", {u1: [0, 1, 2, 4, 7], u2:[4, 5, 8], d1:[0, 1, 2, 3, 5, 8], d2:[3, 5, 7]});    // <
     tiles[7] = createImage("7_curved_road_3_tile", {u1: [3, 6, 8], u2:[4, 5, 8], d1:[0, 1, 2, 3, 5, 8], d2:[0, 1, 2, 4, 6, 8]});    // v
-    tiles[8] = createImage("8_curved_road_4_tile", {u1: [0, 1, 2, 4, 6, 8], u2:[0, 1, 2, 3, 6, 7], d1:[4, 6, 7], d2:[3, 5, 7]});    // ^
+    tiles[8] = createImage("8_curved_road_4_tile", {u1: [0, 1, 2, 4, 7], u2:[0, 1, 2, 3, 6, 7], d1:[4, 6, 7], d2:[3, 5, 7]});    // ^
 }
 
 // Retrieve an image from a file path
@@ -66,6 +83,10 @@ function windowResize() {
   canvas.width  = window.innerWidth;
   canvas.height = window.innerHeight;
   fillGrid();
+};
+
+function randomArrayChoice(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
 };
 
 // Main function
