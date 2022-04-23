e768e4d0492aaef1ee559788a0590aad404b179f
24Chrome <m.batt1@outlook.com>
2022-04-23 00:17:59 -0500

    added comments

--- scripts/main.js
+++ scripts/main.js
@@ -219,10 +219,10 @@ function populateTiles() {
     
     tiles[9] = createImage("9_fourway_road_tile", {u1: [3, 6, 8], u2:[4, 5, 8], d1:[4, 6, 7], d2:[3, 5, 7]});  // /
     
-    tiles[10] = createImage("10_t_road_1_tile", {u1: upLeftRoad, u2:upRightGrass, d1:downLeftRoad, d2:downRightGrass});
-    tiles[11] = createImage("11_t_road_2_tile", {u1: upLeftGrass, u2:upRightRoad, d1:downLeftGrass, d2:downRightRoad});
-    tiles[12] = createImage("12_t_road_3_tile", {u1: upLeftRoad, u2:upRightRoad, d1:downLeftGrass, d2:downRightGrass});
-    tiles[13] = createImage("13_t_road_4_tile", {u1: upLeftGrass, u2:upRightGrass, d1:downLeftRoad, d2:downRightRoad});
+    tiles[10] = createImage("10_t_road_1_tile", {u1: upLeftRoad, u2:upRightGrass, d1:downLeftRoad, d2:downRightGrass});  //
+    tiles[11] = createImage("11_t_road_2_tile", {u1: upLeftGrass, u2:upRightRoad, d1:downLeftGrass, d2:downRightRoad});  //
+    tiles[12] = createImage("12_t_road_3_tile", {u1: upLeftRoad, u2:upRightRoad, d1:downLeftGrass, d2:downRightGrass});  //
+    tiles[13] = createImage("13_t_road_4_tile", {u1: upLeftGrass, u2:upRightGrass, d1:downLeftRoad, d2:downRightRoad});  //
     
     tiles[99] = createImage("bad_tile", {u1:[], u2:[], d1:[], d2:[]});
 }
